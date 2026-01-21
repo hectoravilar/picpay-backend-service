3d35ff1a87bd51d067da63819b67450f0600a820
Hector Avila <hectoravllr@gmail.com>
2026-01-20 21:34:40 -0300

    setup: establish Spring Boot to MySQL communication

--- src/main/java/hector/avlr/picpay/PicpayApplication.java
+++ src/main/java/hector/avlr/picpay/PicpayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PicpayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PicpayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PicpayApplication.class, args);
+    }
 
 }
